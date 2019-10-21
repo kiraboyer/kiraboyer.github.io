dc1c668fa30cc6776035f477c2cc0f6b2573c9c7
kiraboyer <47238965+kiraboyer@users.noreply.github.com>
2019-10-21 02:58:58 -0700

    b

---
+++ art175_website2/js/password.js
@@ -0,0 +1,14 @@
+ 
+
+
+ function Pswd() {
+        var confirmPassword = "mary";
+        var password = document.getElementById("pswd").value;
+        if (password == confirmPassword) {
+             window.location="../art175_website2/page2.html";
+        }
+        else{
+            alert("she was innocent.");
+        }
+    }
+ 
